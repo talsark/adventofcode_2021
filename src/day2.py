acba4bf4c63b574742e7b35cb8fd45dc2ce73c9e
talsark <16123835+talsark@users.noreply.github.com>
2021-12-03 13:06:22 -0800

    Day 2: Dive! - Part 2

--- src/day2.py
+++ src/day2.py
@@ -23,3 +23,28 @@ for command in commands:
 dive = horizontal_position * depth
 
 print(f"Part 1: {dive}")
+
+"""Part 2.
+forward: add to horizontal position
+forward: add aim multiplied by amount to depth
+down: add to aim
+up: subtract from aim
+new dive: total horizontal position multiplied by total depth
+"""
+
+horizontal_position_new = 0
+depth_new = 0
+aim = 0
+for command in commands:
+    if command[0] == "forward":
+        horizontal_position_new = horizontal_position_new + int(command[1])
+        depth_new = depth_new + (aim * int(command[1]))
+    elif command[0] == "down":
+        aim = aim + int(command[1])
+    elif command[0] == "up":
+        aim = aim - int(command[1])
+    else:
+        print("uh oh")
+dive_new = horizontal_position_new * depth_new
+
+print(f"Part 2: {dive_new}")
